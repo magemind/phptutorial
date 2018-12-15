b240b8a38b6cf991a159793dee5c004b0d22e4be
Timm Felix Kluth <kontakt@magemind.de>
2018-12-15 15:50:40 +0100

    Initial commit - added gitignore and index

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+header("Content-Type: text/plain");
+/**
+ * Created by PhpStorm.
+ * User: timm kluth
+ *
+ * Auf der Suche nach einem Webdesign Freelancer? Als Webdesign Freelancer mit über 13 Jahren Programmiererfahrung, beherrsche ich sowohl alle gängigen Frontend- wie Backend Technologien:
+ *
+ * PHP
+ * xHTML
+ * JavaScript
+ * jQuery
+ * CSS3
+ * SQL
+ * Das Entwickeln in agilen Teams nach SCRUM, sowie Deployment-Szenarien mit GIT sind mir geläufig, weshalb mit geringer Einarbeitungszeit in Ihr Team zu rechnen ist.
+ * RUFEN SIE MICH AN
+ *
+ * 0176 54 85 3861
+ * kontakt@magemind.de
+ * Website: https://www.magemind.de/
+ *
+ *
+ *
+ */
+ ?>
+<html>
+  <head></head>
+  <body>
+  <div>
+    <h2>1. Definiere eine Klasse</h2>
+      <p></p>
+  </div>
+</body>
+</html>
